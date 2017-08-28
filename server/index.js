37b76a04bbb127f5b48af6b4ba439153928ea6f2
Dee Foster <dee3724f@gmail.com>
2017-08-28 12:03:13 -0600

    ready for live

--- server/index.js
+++ server/index.js
@@ -3,12 +3,13 @@ var mongoose = require("mongoose");
 var bodyParser = require('body-parser')
 var cors = require('cors');
 var dbConnect = require('./config/db/mlab-config')
-var port = 3000
+// var port = 3000
+var port = process.env.PORT || 3000;
 
 var server = express();
 server.use(cors());
 
-//server.use(express.static(__dirname + '/public' ))
+server.use(express.static(__dirname + '/../public/dist' )) //might need leading '/'
 server.use(bodyParser.json())
 server.use(bodyParser.urlencoded({extended:true}))
 
